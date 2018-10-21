93754ec0c309c619c780e8776b244d2d3b69e25b
rene <renegmed@yahoo.com>
2018-10-21 13:53:49 -0400

    added search feature of command cli

---
+++ sec05-design-cli/cmd/pork/main.go
@@ -0,0 +1,29 @@
+package main
+
+/*
+
+	$ go install ./cmd/pork
+	$ pork search topic:ruby
+
+
+*/
+
+import (
+	"devops_tools/sec05-design-cli"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd *cobra.Command
+
+func main() {
+	rootCmd.Execute()
+}
+
+func init() {
+	rootCmd = &cobra.Command{
+		Use:   "pork",
+		Short: "Project Forking Tool for Github",
+	}
+	rootCmd.AddCommand(pork.SearchCmd)
+}
